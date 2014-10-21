ec2a9be39ebcb318b107aa6fac59589a30576f75
Dillon Zhang <d1998z@gmail.com>
2014-10-20 22:58:12 -0400

    Driver Update

--- Driver.java
+++ Driver.java
@@ -20,26 +20,56 @@ public class Driver {
        	Scanner sc = new Scanner(System.in);
 	Random r = new Random();
 	String s="";
+
 	while (!s.equals("done")) {
+
+	    /* Is this needed?
 	    System.out.print("Input: ");
 	    s = sc.nextLine();
 	    System.out.println("Your input: "+s);
+	    */
+	    
+
 	    //creates player type
-	    System.out.println("Warrior or Mage?");
+	    System.out.print("Warrior or Mage?");
 	    s=sc.nextLine();
+
 	    //but as of now doesn't seem to actually create new object
-	    if (s.equals("Warrior")) {
-		Warrior player = new Warrior();
-	    } else if (s.equals("Mage")) {
-		Mage player = new Mage();
-	    } else {
-		System.out.println("Invalid name; choose from 'Warrior' or 'Mage'");
+	    Boolean failure = True;
+
+	    while (failure) {
+		if (s.equals("Warrior")) {
+		    Warrior player = new Warrior();
+		    failure = False;
+		} else if (s.equals("Mage")) {
+		    Mage player = new Mage();
+		    failure = False;
+		} else {
+		    System.out.print("Invalid name; choose from 'Warrior' or 'Mage'");
+		}
 	    }
+
 	    //choosing name
 	    System.out.println("Choose your name!");
 	    s = sc.nextLine();
+
 	    player.setName(s);
 	    System.out.println("Your name: "+s);
-	    }	    
+	
+
+	    //Needs list of all possible actions to perform
+	    System.out.println("What would you like to do (LIST): ");
+	    
+	    String playerchoice = sc.nextLine();
+	    //Tedious if statements for user side
+
+	    //Depends on the number of actions we want
+	    int computerchoice = r.nextInt(9);
+
+	    //Tedious if statements for computers action
+
+
+	    
+	}	    
     }
 }
