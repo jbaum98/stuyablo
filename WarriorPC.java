60c899f0ff1f1d3f3a86d407e9afe6b5fc60c338
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-10-21 11:47:37 -0400

    removed PC classes

--- WarriorPC.java
+++
@@ -1,9 +0,0 @@
-public class WarriorPC extends Warrior {
-    public WarriorPC() {
-	super();
-	this.setHealth(randomFromTo(100,500));
-	this.setMana(randomFromTo(100,500));
-	this.setLevel(randomFromTo(1,20));
-	this.setName("Monster");
-    }
-}
