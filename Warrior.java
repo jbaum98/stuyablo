be70632c464ab6890c52b85554573f4b9a7c9183
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-10-21 11:47:20 -0400

    fixed warrior with setters

--- Warrior.java
+++ Warrior.java
@@ -1,38 +1,36 @@
 public class Warrior extends BaseChar {
   public Warrior() {
       super();
-      this.setPAttack(this.getPAttack() * 1.50);
-      this.setPDefense(this.getPAttack() * 1.50);
+      setPAttack(getPAttack() * 1.50);
+      setPDefense(getPAttack() * 1.50);
   }
 
   public Warrior(String name) {
       super();
-      this.setPAttack(this.getPAttack() * 1.50);
-      this.setPDefense(this.getPAttack() * 1.50);
+      setPAttack(getPAttack() * 1.50);
+      setPDefense(getPAttack() * 1.50);
   }
   //attributes
 
   //methods
   public void getPumped(){
     removeMana(30);
-      this.setSpeedMod(this.getSpeedMod() + 20);
-      this.setPDefense(this.getPAttack() + 30);
-    this.speedMod = 20;
-    this.pAttackMod = 20;
+      setSpeedMod(getSpeedMod() + 20);
+      setPDefense(getPAttack() + 30);
   }
 
   public void Discombobulate(BaseChar other){
     removeMana(50);
-    other.speedMod = -20;
-    other.accuracyMod = -20;
+    other.setSpeedMod(other.getSpeedMod() -20);
+    other.setAccuracyMod(other.getSpeedMod() -20);
   }
 
   public void sneakAttack(BaseChar other){
-    int a = other.pDefenseMod;
+    int d = other.pDefenseMod;
     removeMana(40);
-    other.pDefenseMod = -1*other.getPDefense();
+    other.setPDefenseMod(other.getSpeedMod(-1*other.getPDefense());
     baseAttack(other);
-    other.pDefenseMod = a;
+    other.setPDefenseMod(d);
   }
 
   public void doubleStrike(BaseChar other){
@@ -43,9 +41,10 @@ public class Warrior extends BaseChar {
 
   public void Finisher(BaseChar other){
     removeMana(60);
-    this.pAttackMod = getPAttack();
+    int a = getPAttackMod();
+    setPAttackMod(getPAttack());
     baseAttack(other);
     baseAttack(other); 
-    this.pAttackMod = 0;
+    pAttackMod = a;
   }
 }
